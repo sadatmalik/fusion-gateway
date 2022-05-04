cf47421de9499d53c6678b4e0e0e5aa043c52645
sadat.malik <sadat.malik@me.com>
2022-05-04 10:09:40 +0100

    spring.io initial project files

---
+++ src/main/java/com/sadatmalik/fusiongateway/FusionGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.fusiongateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionGatewayApplication.class, args);
+	}
+
+}
