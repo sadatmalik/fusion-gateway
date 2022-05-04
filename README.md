e0ea0ba1394c28571624be3b16ff93da823bc6e9
sadat.malik <sadat.malik@me.com>
2022-05-04 10:36:10 +0100

    updating javadoc and readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# fusion-gateway
+# Fusion Gateway
+
+To see the routes managed by the Gateway server, we can list the routes via the `actuator/gateway/routes` endpoint 
+on the Gateway server.
--- src/main/java/com/sadatmalik/fusiongateway/FusionGatewayApplication.java
+++ src/main/java/com/sadatmalik/fusiongateway/FusionGatewayApplication.java
@@ -2,8 +2,21 @@ package com.sadatmalik.fusiongateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Bootstrap class starts a Fusion Api Gateway enabled as a eureka client.
+ *
+ * Using Spring Cloud Gateway with Eureka means we have the standard gateway a single endpoint
+ * through which we can make calls, and also we can add and remove instances of a service without
+ * ever having to modify the gateway. For instance, we can add a new service to Eureka, and the
+ * gateway automatically routes calls to it because it’s communicating with Eureka about where the
+ * actual physical service endpoints are located.
+ *
+ * @author sadatmalik
+ */
 @SpringBootApplication
+@EnableEurekaClient
 public class FusionGatewayApplication {
 
 	public static void main(String[] args) {
